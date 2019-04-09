c6605a5e6531eaad9726b92725ce55ed501176c6
Avichail <avichailbenjamin@gmail.com>
2019-04-10 00:05:18 +0300

    app-layout setting

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import styled from '@emotion/styled'
+
+// component
+import Dashboard from './components/pages/dashboard/Dashboard'
+import Chat from './components/pages/chat/Chat'
+import Deals from './components/pages/deals/Deals'
+import Helper from './components/pages/helper/Helper'
+import Header from './components/Widgets/header/Header'
+import Menu from './components/Widgets/menu/Menu'
 
 
 class App extends Component {
     render() {
-      return (
-        <div className="App">
-            App
-        </div>
-      );
-    }
+      	return (
+			<BrowserRouter>
+				<Container>
+					<Header/>
+					<MainScreen>
+						<Menu/>
+						<Route path='/dashboard' render={() => <Dashboard/>}/>
+						<Route path='/chat' render={() => <Chat/>}/>
+						<Route path='/deals' render={() => <Deals/>}/>
+						<Route path='/helper' render={() => <Helper/>}/>
+					</MainScreen>
+				</Container>
+			</BrowserRouter>
+      	);
+   	}
 }
 
 export default App
+
+const Container = styled.div`
+
+`
+
+const MainScreen = styled.div`
+`
+// display: flex;
---
+++ web-react/src/components/pages/dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+
+const Dashboard = () => {
+  return (
+    <Container>
+      dashboard
+    </Container>
+  )
+}
+
+export default Dashboard
+
+const Container = styled.div`
+    margin: 56px 0 0 170px;
+    height: 100vh;
+    background: red;
+`
